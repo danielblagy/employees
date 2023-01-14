4a980d38a2a14033780e34dfb16fde692b48a72f
Daniel Blagy <daniel.blagy@gmail.com>
2023-01-14 16:17:18 +0300

    add simple test http server

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func welcome(response http.ResponseWriter, request *http.Request) {
+	log.Println("/ is requested")
+	fmt.Fprintf(response, "Welcome to employees API!")
+}
 
 func main() {
-	fmt.Println("employees")
+	fmt.Println("starting employees server locally on port 5000")
+
+	http.HandleFunc("/", welcome)
+
+	log.Fatal(http.ListenAndServe(":5000", nil))
 }
